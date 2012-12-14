b9d6b8416b601a71d339534a82fb3ee4d1be7077
importre <jaeweheo@gmail.com>
2012-12-14 22:00:15 +0900

    initial commit

---
+++ importre-viewcontroller/importre-viewcontroller/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  importre-viewcontroller
+//
+//  Created by importre on 12. 12. 14..
+//  Copyright (c) 2012년 AhnLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ importre-viewcontroller/importre-viewcontroller/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  importre-viewcontroller
+//
+//  Created by importre on 12. 12. 14..
+//  Copyright (c) 2012년 AhnLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
