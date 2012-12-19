6e59a1864d78cd7f2b3e09c83189f4d70f8621e7
importre <jaeweheo@gmail.com>
2012-12-19 20:05:59 +0900

    init commit

---
+++ importre-scrollview/importre-scrollview/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  importre-scrollview
+//
+//  Created by importre on 12. 12. 19..
+//  Copyright (c) 2012년 AhnLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ importre-scrollview/importre-scrollview/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  importre-scrollview
+//
+//  Created by importre on 12. 12. 19..
+//  Copyright (c) 2012년 AhnLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
