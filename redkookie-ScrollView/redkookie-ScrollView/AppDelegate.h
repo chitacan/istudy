30650953929e55d772b368b8caca9561b67d4622
cloody <cloody@cloodyui-MacBook-Pro.local>
2012-12-24 23:22:21 +0900

    임시 커밋

---
+++ redkookie-ScrollView/redkookie-ScrollView/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  redkookie-ScrollView
+//
+//  Created by Kim cloody on 12. 12. 24..
+//  Copyright (c) 2012 AhnLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
