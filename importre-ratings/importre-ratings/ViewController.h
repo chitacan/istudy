fd0347723d058a683de547100f319a778c6660b2
importre <importre@mba.local>
2012-12-30 00:52:16 +0900

    initial commit

---
+++ importre-ratings/importre-ratings/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  importre-ratings
+//
+//  Created by importre on 12. 12. 30..
+//  Copyright (c) 2012년 AhnLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
