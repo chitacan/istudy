2c9dd78ea9d13f4ebc34b33f1e6858f7a702f614
redkookie <redkookie@gmail.com>
2013-01-01 22:00:01 +0900

    간단한 스토리보드 연습 (테이블뷰 + 네비게이션 컨트롤)

---
+++ redkookie-RecipeBook/redkookie-RecipeBook/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  redkookie-RecipeBook
+//
+//  Created by cloody on 13. 1. 1..
+//  Copyright (c) 2013년 cloody. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
