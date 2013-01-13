48578d0ad84d14da9ec7022ab8babcaf91605053
redkookie <redkookie@gmail.com>
2013-01-13 16:18:52 +0900

    iOS6 AutoLayout complete

---
+++ redkookie-Constraints/redkookie-Constraints/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  redkookie-Constraints
+//
+//  Created by cloody on 13. 1. 13..
+//  Copyright (c) 2013년 cloody. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+- (IBAction)buttonTapped:(UIButton *)sender;
+
+@end
